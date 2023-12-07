bfd4fef3ec216cd58a60855f2eb86a08a9ab384d
Usnagiri Srikanth <UsnagiriS@jmf.com>
2023-12-07 11:24:21 +0530

    dise created

--- scr/main/screens/PlayGameScreen.js
+++ scr/main/screens/PlayGameScreen.js
@@ -1,9 +1,11 @@
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, TouchableWithoutFeedback} from 'react-native';
 import React, {useEffect, useState} from 'react';
 
 const PlayGameScreen = () => {
   const [commObj, setCommObj] = useState({
     NumbersArray: [],
+    player: 'p1',
+    diseNumber: 1,
   });
 
   useEffect(() => {
@@ -25,6 +27,28 @@ const PlayGameScreen = () => {
       NumbersArray: result.reverse(),
     }));
   }, []);
+
+  function disePlayer1Hanlder() {
+    let arr = [1, 2, 3, 4, 5, 6];
+    let randomNum = arr[Math.floor(Math.random() * arr.length)];
+
+    setCommObj(prev => ({
+      ...prev,
+      diseNumber: randomNum,
+      player: 'p2',
+    }));
+  }
+
+  function disePlayer2Hanlder() {
+    let arr = [1, 2, 3, 4, 5, 6];
+    let randomNum = arr[Math.floor(Math.random() * arr.length)];
+
+    setCommObj(prev => ({
+      ...prev,
+      diseNumber: randomNum,
+      player: 'p1',
+    }));
+  }
 
   useEffect(() => {
     console.log('PlayGameScreen-commObj------>', commObj);
@@ -78,6 +102,67 @@ const PlayGameScreen = () => {
                 </View>
               );
             })}
+        </View>
+        <View
+          style={{
+            paddingTop: 50,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              backgroundColor: 'lightblue',
+              justifyContent: 'space-between',
+              paddingHorizontal: 20,
+              paddingVertical: 5,
+              alignItems: 'center',
+            }}>
+            <View>
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontWeight: commObj.player == 'p1' ? 'bold' : '500',
+                  color: commObj.player == 'p1' ? 'blue' : '#000',
+                }}>
+                Player 1
+              </Text>
+            </View>
+            <TouchableWithoutFeedback
+              onPress={() => {
+                if (commObj.player == 'p1') {
+                  disePlayer1Hanlder();
+                }
+                if (commObj.player == 'p2') {
+                  disePlayer2Hanlder();
+                }
+              }}>
+              <View
+                style={{
+                  backgroundColor: '#fff',
+                  width: 70,
+                  paddingVertical: 5,
+                  alignItems: 'center',
+                }}>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    color: commObj.player == 'p1' ? 'blue' : 'red',
+                    fontWeight: 'bold',
+                  }}>
+                  {commObj.diseNumber}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <View>
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontWeight: commObj.player == 'p2' ? 'bold' : '500',
+                  color: commObj.player == 'p2' ? 'red' : '#000',
+                }}>
+                Player 2
+              </Text>
+            </View>
+          </View>
         </View>
       </View>
     </SafeAreaView>
